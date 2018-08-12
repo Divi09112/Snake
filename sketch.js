550a1d585f995f012ca3be869deec51ea5de587a
Divyansh <divi09112@gmail.com>
2018-08-12 17:17:22 +0530

    setup

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+var s;
+var side = 20;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(51);
+	s = new Snake();
+	frameRate = 20;
 }
 
 function draw() {
+	s.show();
+}
+
+function Snake() {
+	this.x = 2;
+	this.y = 2;
+	this.side = side;
+	this.speedx = 0;
+	this.speedy = 0;
+
+	this.show = function () {
+		rect(this.x, this.y, side, side);
+	}
 
 }
